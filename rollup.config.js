21694a3deef718a1566e5a263c02e8f7ca725d8b
jax <zhouhuan@mchz.com.cn>
2018-10-30 17:21:50 +0800

    更新 rolup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -4,14 +4,14 @@ import commonjs from "rollup-plugin-commonjs"
 import nodeResolve from "rollup-plugin-node-resolve"
 import { sizeSnapshot } from "rollup-plugin-size-snapshot"
 
-
 import pkg from "./package.json"
 const name = "CRUD";
 const input = "./modules/index"
 
 
 const globals = {
-  react: "React"
+  react: "React",
+  "prop-types":"PropTypes"
 };
 const babelOptionsCJS = {
   exclude: /node_modules/
@@ -24,13 +24,13 @@ const babelOptionsESM = {
 const commonjsOptions = {
   include: /node_modules/
 };
-const external = id => !id.startsWith(".") && !id.startsWith("/");
+const external = id => !id.startsWith(".") && !id.startsWith("\/");
 
 export default [{
   input,
   output: { file: `esm/${pkg.name}.js`, format: "esm" },
-  external,
-  plugins: [nodeResolve(),babel(babelOptionsESM)]
+  external:Object.keys(globals),
+  plugins: [nodeResolve(),babel(babelOptionsESM), sizeSnapshot()]
 },{
    input,
    output: { file: `umd/${pkg.name}.js`, format: "umd", name, globals },
