7519e5267fa1a68d1e1dcdc060eed71f92a3155f
龙鸿轩 <ignacio4@126.com>
2018-12-12 13:26:13 +0800

    add viewpage

--- modules/index.js
+++ modules/index.js
@@ -1,6 +1,7 @@
 import { default as ListPage } from './ListPage'
 import { default as FormPage } from './FormPage'
 import { default as ModalAndView } from './ModalAndView'
+import { default as ViewPage } from './Page'
 
-export {ListPage,FormPage,ModalAndView}
+export { ListPage, FormPage, ModalAndView, ViewPage }
 export default ListPage
