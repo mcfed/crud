2d289973fcb33f9e53b3bb56471991faf6b82c37
jaxchow <jaxchow@gmail.com>
2020-05-06 10:04:31 +0800

    feat: 移除vue编译代码

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@ import RViewPage from './Page/Page.react'
 
 
 
-import { default as VListPage } from './ListPage/List.vue'
-import { default as VFormPage } from './FormPage/Form.vue'
-import { default as VViewPage } from './Page/Page.vue'
+// import { default as VListPage } from './ListPage/List.vue'
+// import { default as VFormPage } from './FormPage/Form.vue'
+// import { default as VViewPage } from './Page/Page.vue'
 
 export * from './Page/IPage'
 export * from './FormPage/IForm'
 export * from './ListPage/IList'
 
-export { VListPage, VFormPage, VViewPage,RListPage, RFormPage, RViewPage}
+export { /*VListPage, VFormPage, VViewPage,*/RListPage, RFormPage, RViewPage}
 export { RListPage as ListPage, RFormPage as FormPage, RViewPage as ViewPage }
